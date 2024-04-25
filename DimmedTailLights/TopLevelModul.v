8db6f91dfc292af0b45cb4dea5317e6d120f6443
Nico Bachmann <103788651+nico1645@users.noreply.github.com>
2024-04-25 11:29:42 +0200

    Fix timitng DimmedTailLights

--- DimmedTailLights/TopLevelModul.v
+++ DimmedTailLights/TopLevelModul.v
@@ -2,7 +2,7 @@
 module TopLevelModul(input clk, reset, left, right, output [5:0] lights);
 wire clk_en;
 
-ClkDiv #(.EXP(20)) clk_div (
+ClkDiv #(.EXP(24)) clk_div (
     .clk(clk),
     .rst(reset),
     .clk_en(clk_en)
