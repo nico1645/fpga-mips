9bd561aedc2b06df3a3ee20cfc738b77af0d8e26
Nico Bachmann <103788651+nico1645@users.noreply.github.com>
2024-05-11 14:31:44 +0200

    Solved Lab 7 Report

--- lab7/color_func.asm
+++ lab7/color_func.asm
@@ -1,8 +1,18 @@
-abs_diff_color:                         # abs_diff_color
-        lw $s4, 0($sp)		# Loading argument 4
-        lw $s5, 4($sp)		# Loading argument 5
-        addi   $sp, $sp, -4	# Pushing $ra into stack
-        sw      $ra, 4($sp)
+.data				#Function arguments inputs
+	R1: .word 1
+	G1: .word 2
+	B1: .word 3
+	R2: .word 4
+	G2: .word 5
+	B2: .word 6
+.text
+_start:
+	j main
+abs_diff_color:                 # abs_diff_color
+        lw $s4, 4($sp)		# Loading argument 4
+        lw $s5, 0($sp)		# Loading argument 5
+        addi $sp, $sp, -4	# Pushing $ra into stack
+        sw $ra, 0($sp)
         move $s7, $zero		# Variable for summing persistent between function calls
         move $s0, $a0		# Persist between function call
         move $s1, $a1		# Persist between function call
@@ -22,23 +32,23 @@ abs_diff_color:                         # abs_diff_color
         add $s7, $s7, $v0	# Adding abs(B1-B2) to the total sum
         
         move $v0, $s7		# Set $v0 to the contents of $s7
-        lw	$ra, 4($sp)	# Retrieve $ra from stack
-        addi   $sp, $sp, 24
-        jr      $ra
+        lw $ra, 0($sp)		# Retrieve $ra from stack
+        addi $sp, $sp, 4
+        jr $ra
 abs:
 	sra $t0,$a0,31    # Filled with 0xFFFFFFF if negative else 0x0000000
 	xor $v0,$a0,$t0   # Flipping bits if negative number
 	sub $v0,$v0,$t0   # Subtracting 1 if it is negative complement
 	jr $ra
 main:                                   # Assuming function is called with the following  arguments abs_diff_color(1,2,3,4,5,6)
-        addi   $sp, $sp, -8		# Space for the the last two arguments passed via stack
-        addi   $t1, $zero, 6
-        sw      $t1, 0($sp)
-        addi   $t1, $zero, 5
-        sw      $t1, 4($sp)
-        addi   $4, $zero, 1
-        addi   $5, $zero, 2
-        addi   $6, $zero, 3
-        addi   $7, $zero, 4
-        jal     abs_diff_color
-        addi   $sp, $sp, 8
+        addi $sp, $sp, -8		# Space for the the last two arguments passed via stack
+        lw $t1, G2
+        lw $t2, B2
+        sw $t1, 4($sp)
+        sw $t2, 0($sp)
+        lw $4 R1
+        lw $5 G1
+        lw $6 B1
+        lw $7, R2
+        jal abs_diff_color
+        addi $sp, $sp, 8
